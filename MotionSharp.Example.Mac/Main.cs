f257aac607e5ec3ce05637fa6f01b7d71f5f930a
Jose Medrano <josmed@microsoft.com>
2019-07-14 20:06:56 +0200

    adds initial example

---
+++ MotionSharp.Example.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace MotionSharp.Example.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
